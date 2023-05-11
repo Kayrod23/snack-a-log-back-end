fc3a023400706b230c8bc748c03d02c7a6727599
ryanfodera <ryanfodera@pursuit.org>
2023-05-10 23:17:16 -0400

    update and delete

--- queries/snacks.js
+++ queries/snacks.js
@@ -30,8 +30,32 @@ const createSnack = async (snack) => {
     };
 };
 
+const updateSnack = async (id, snack) => {
+    try {
+        const updatedSnack = await db.one(
+            "UPDATE snacks SET name=$1, image=$2, protein=$3, fiber=$4, added_sugar=$5, sodium=$6, is_favorite=$7 WHERE id=$8 RETURNING *",
+            [snack.name, snack.image, snack.protein, snack.fiber, snack.added_sugar, snack.sodium, snack.is_favorite, id]
+        );
+        return updatedSnack;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteSnack = async (id) => {
+    try {
+        const deletedSnack = await db.one("DELETE FROM snacks WHERE id=$1 RETURNING *", [id]);
+        return deletedSnack;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
 module.exports = {
     getAllSnacks,
     getSnack,
     createSnack,
-}
+    updateSnack,
+    deleteSnack,
+};
